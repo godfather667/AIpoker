034f9a44cc6f0761183a9f91b7bc36b364679cb9
Charles Thornton <hawthornepresscom@gmail.com>
2019-03-25 21:25:38 -0500

    Initial Commit of AIpoker Game -

---
+++ AIpoker.go
@@ -0,0 +1,49 @@
+//
+// Display Table
+//
+package main
+
+import (
+	"fmt"
+	_ "image/png"
+	"log"
+
+	"github.com/hajimehoshi/ebiten"
+	"github.com/hajimehoshi/ebiten/ebitenutil"
+)
+
+var img *ebiten.Image
+
+func init() {
+	var err error
+	img, _, err = ebitenutil.NewImageFromFile("images/table.png", ebiten.FilterDefault)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func update(screen *ebiten.Image) error {
+	if ebiten.IsDrawingSkipped() {
+		return nil
+	}
+	screen.DrawImage(img, nil)
+	return nil
+}
+
+func instruct() {
+	fmt.Println("\n                        A I  P O K E R")
+	fmt.Println("\nAIpoker is the initial concept design for an AI Based Poker Simulation")
+	fmt.Println("Currently it only displays a empty table.")
+	fmt.Println("\nHowever, the 'images' directory contains all the cards and other necessary images.")
+	fmt.Println("Still a huge amount of work to do.\n")
+	fmt.Println("Only one 'bug' at the moment: You must put the cursor in the image box for")
+	fmt.Println("the entire image to be completely displayed.")
+	fmt.Println("\nThis 'Feature' also shows up in their example games. May take Big Think!")
+}
+
+func main() {
+	instruct() // Display current notes on project progress
+	if err := ebiten.Run(update, 1024, 512, 1, "AI POKER - You against 9 AI Players!"); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# AIpoker
+**AIpoker** is the initial concept design for an _**AI Based Poker Simulation**_.
+
+The goal is to allow a human player to test their skills against eight AI players in Texas Holdem.
+
+The user will be able to select a variety of AI players of different skills levels.
+
+It also serves as a test bed for the **github.com/hajimehoshi/ebiten** Dislay Package.
+
+* Currently it only displays a empty table.
+* The 'images' directory contains all the cards and other necessary images.
+* There is a BUG in that *the cursor must be in the initial display box* for the complete image to be displayed. 
+* Since the BUG also in the example games I have compiled. The fix is in by TODO List.
